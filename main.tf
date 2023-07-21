61c5a734911ec6773cae1d8b291e7b829fcf55ed
Kshitij Lingthep <35816754+Anonymous-Chris@users.noreply.github.com>
2023-07-21 11:51:54 -0500

    update previous one

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
   
   //step 6
-  vpc_security_groups_id = [aws_security_group.blog.id]
+  vpc_security_groups_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
